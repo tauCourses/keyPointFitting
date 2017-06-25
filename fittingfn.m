4864fda789ba7003237ef6d17ac1999f96879a23
itay itzhak <Itayit>
2017-06-25 23:59:43 +0300

    start of ransac

---
+++ fittingfn.m
@@ -0,0 +1,15 @@
+function [M] = fittingfn(x)
+%     fittingfn - Handle to a function that fits a model to s
+%                 data from x.  It is assumed that the function is of the
+%                 form: 
+%                    M = fittingfn(x)
+%                 Note it is possible that the fitting function can return
+%                 multiple models (for example up to 3 fundamental matrices
+%                 can be fitted to 7 matched points).  In this case it is
+%                 assumed that the fitting function returns a cell array of
+%                 models.
+%                 If this function cannot fit a model it should return M as
+%                 an empty matrix.
+    M = DLT(x');
+end
+
