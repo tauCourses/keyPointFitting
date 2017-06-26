df5e52d5c5aa869d9af73905a791e33b9e48deb2
itay itzhak <Itayit>
2017-06-26 19:58:54 +0300

    normalize in wrapper

--- RANSAC_Wrapper.m
+++ RANSAC_Wrapper.m
@@ -1,5 +1,10 @@
 function [H_ransac] = RANSAC_Wrapper(matches, fittingfn,distfn, degenfn, s, t, feedback, maxDataTrials,maxTrials)
 
-tranMatches = matches';
-[h,inliners] = ransac(tranMatches,fittingfn,distfn,degenfn, s, t, feedback, maxDataTrials,maxTrials);
-H_ransac = h;
+[T1,T2] = normlizePoints(matches);
+
+tranAndNormalMatches(:,1:3) = T1;
+tranAndNormalMatches(:,4:6) = T2;
+tranAndNormalMatches = (tranAndNormalMatches)'; 
+
+[H_ransac,inliners] = ransac(tranAndNormalMatches,fittingfn,distfn,degenfn, s, t, feedback, maxDataTrials,maxTrials);
+H_ransac = (inv(T2) * H_ransac * T1)';
