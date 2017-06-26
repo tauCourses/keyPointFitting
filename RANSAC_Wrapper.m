4bc11aa5f459de137d8b65f0a67604f621948a75
MatanLed <matan.harel.mh@gmail.com>
2017-06-26 20:29:22 +0300

    merged

--- RANSAC_Wrapper.m
+++ RANSAC_Wrapper.m
@@ -1,10 +1,10 @@
 function [H_ransac] = RANSAC_Wrapper(matches, fittingfn,distfn, degenfn, s, t, feedback, maxDataTrials,maxTrials)
 
-[T1,T2] = normlizePoints(matches);
+%[T1,T2] = normlizePoints(matches);
 
-tranAndNormalMatches(:,1:3) = T1;
-tranAndNormalMatches(:,4:6) = T2;
-tranAndNormalMatches = (tranAndNormalMatches)'; 
+%tranAndNormalMatches(:,1:3) = T1;
+%tranAndNormalMatches(:,4:6) = T2;
+tranAndNormalMatches = (matches)'; 
 
 [H_ransac,inliners] = ransac(tranAndNormalMatches,fittingfn,distfn,degenfn, s, t, feedback, maxDataTrials,maxTrials);
-H_ransac = (inv(T2) * H_ransac * T1)';
+%H_ransac = (inv(T2) * H_ransac * T1)';
