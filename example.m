6b202338798c0bee0530d3c90ebfbbfc55881448
MatanLed <matan.harel.mh@gmail.com>
2017-06-27 20:49:46 +0300

    example merged

--- example.m
+++ example.m
@@ -1,36 +1,12 @@
-<<<<<<< HEAD
-firstAddress = 'D:\Tel aviv university\Graphics\Assignment3\me1.pgm';
-secondAddress =  'D:\Tel aviv university\Graphics\Assignment3\me2.pgm';
-minGroup = 4;
-inlierTrashold = 2;
-=======
-firstAddress = 'D:\targil 3\left.pgm';
-secondAddress =  'D:\targil 3\example.pgm';
+ss = 'D:\Tel aviv university\Graphics\Assignment3\left.pgm';
+secondAddress =  'D:\Tel aviv university\Graphics\Assignment3\example.pgm';
 minGroup = 10;
 inlierTrashold = 0.5;
->>>>>>> origin/noramlRansac
 
 H=[1 .4 0; .1 1 0; 0.5 0.3 0.9];
 
 oriImg = imread(firstAddress);
-<<<<<<< HEAD
-afterImg = imread(secondAddress); %ComputeProjective(oriImg, H);
-%imwrite(afterImg, secondAddress);
 
-[num matches distVals] = match(firstAddress,secondAddress, 0.8);
-DisplayCorr(firstAddress,secondAddress,matches,distVals,10)
-% computedH = RANSAC_Wrapper(matches,@fittingfn,@myDistfn,@degenfn,minGroup,inlierTrashold,0,100,2000);
-% 
-% calcImg = ComputeProjective(oriImg, computedH);
-% diffImg = appendimages(afterImg,calcImg);
-% 
-% %[pnts_gt,pnts_computed] = ComputeTestPoints(H,computedH);
-% %error = ComputeError(pnts_gt,pnts_computed) / size(pnts_gt,1)
-% 
-% sizediff = size(afterImg) - size(calcImg)
-% 
-% imshow(diffImg);
-=======
 afterImg = ComputeProjective(oriImg, H);
 imwrite(afterImg, secondAddress);
 
@@ -47,4 +23,3 @@ error = ComputeError(pnts_gt,pnts_computed) / size(pnts_gt,1)
 sizediff = size(afterImg) - size(calcImg)
 
 imshow(diffImg);
->>>>>>> origin/noramlRansac
